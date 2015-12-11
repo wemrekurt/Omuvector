c795123e858a211cdecb1951834c27b7d729d528
wemrekurt <emre.kurt@bil.omu.edu.tr>
2015-12-11 23:03:28 +0200

    Deleting Subjects

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -21,6 +21,27 @@ class SubjectsController < ApplicationController
       end
   end
 
+  def edit
+    @subject = Subject.find(params[:id])
+  end
+
+  def update
+    @subject = Subject.find(params[:id])
+
+    if @subject.update(params[:subject].permit(:title,:description))
+      redirect_to @subject
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @subject = Subject.find(params[:id])
+    @subject.destroy
+
+    redirect_to root_path
+  end
+
   private
     def subject_params
       params.require(:subject).permit(:title, :description)
