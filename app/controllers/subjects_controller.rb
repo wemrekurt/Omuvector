868c19c2525916582d6f1b9ea9a2de2c44525130
wemrekurt <emre.kurt@bil.omu.edu.tr>
2015-12-11 22:05:59 +0200

    Add Subjects

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,5 +1,25 @@
 class SubjectsController < ApplicationController
 	def index
-		@asd ="s"
-	end
+		@subjects = Subject.all.order('created_at DESC')
+  end
+
+  def new
+
+  end
+
+  def show
+    @subject = Subject.find(params[:id])
+  end
+
+  def create
+    @subject = Subject.new(subject_params)
+    @subject.save
+
+    redirect_to @subject
+  end
+
+  private
+    def subject_params
+      params.require(:subject).permit(:title, :description)
+    end
 end
